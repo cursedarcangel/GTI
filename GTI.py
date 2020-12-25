5831fe2bacde23167ff40f315c33eb0c41bcab6a
cursedarcangel <71993617+cursedarcangel@users.noreply.github.com>
2020-12-25 16:36:47 -0500

    Creating main file

---
+++ GTI.py
@@ -0,0 +1,16 @@
+
+def start():
+    print('Welcome to GTI! What are you looking to do?')
+    print()
+    print('1. Clone a repo from GitHub')
+    print('2. See a list of all changes in a git repo')
+    print('3. Stash or delete changes')
+    print('4. Commit to a repo')
+
+start()
+
+choice = int(input())
+
+if choice == 1:
+    from cmds import clone
+    clone.cloneR()
