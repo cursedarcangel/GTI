23a5309e966a8f04b5b52f9c5399ea3c1381eab3
cursedarcangel <71993617+cursedarcangel@users.noreply.github.com>
2021-01-12 10:49:13 -0500

    Making it so the commit actually works

--- cmds/commit.py
+++ cmds/commit.py
@@ -4,4 +4,4 @@ def commit(repo):
     print('\n' + 'The command for committing is *git commit -m "insert message here"*' + '\n')
     message = input('What should the commit message be?\n')
     os.system('cd ' + repo)
-    os.system('git commit -m ' + message)
+    os.system('git commit -m ' + '"' + message + '"')
