21a7eb1f0744143de2fc423a6fc05182ea006c5d
cursedarcangel <71993617+cursedarcangel@users.noreply.github.com>
2021-01-12 12:54:10 -0500

    Renaming function

--- cmds/clone.py
+++ cmds/clone.py
@@ -1,6 +1,6 @@
 import os
 
-def cloneR():
+def cloneRepo():
     print('\n' + 'The command for cloning a repo is *git clone (insert url here) (insert destination here)*' + '\n')
     url = input('What is the url that you want to clone?\n')
     destination = input('Where do you want the repo to go?\n')
