c959c624b5b7358efea661f467825c5f818dadd8
cursedarcangel <71993617+cursedarcangel@users.noreply.github.com>
2021-01-16 13:01:56 -0500

    Adding specific branch to merge into

--- cmds/merge.py
+++ cmds/merge.py
@@ -2,7 +2,9 @@ import os
 
 def merge(repo):
     print('\n' + 'The command for merging branches is *git merge (branch)*' + '\n')
-    branch = input('What is the name of the branch that you want to merge?')
+    branch = input('What is the name of the branch that you want to merge?\n')
+    branch2 = input('Which branch to merge into?\n')
 
     os.system('cd ' + repo)
+    os.system('git checkout ' + branch2)
     os.system('git merge ' + branch)
